bf0626b0c1feeaff4739fc14c1924cb04af1204d
Anton Fedorovsky <anton.fedorovsky@raiffeisen.ru>
2026-06-07 12:09:20 +0300

    analytics

---
+++ src/__mocks__/gtag.ts
@@ -0,0 +1,13 @@
+export function gaPageView() {}
+export function gaEvent() {}
+export const GAEvents = {
+  login: () => {},
+  register: () => {},
+  logout: () => {},
+  transactionCreated: () => {},
+  transactionUpdated: () => {},
+  transactionDeleted: () => {},
+  categoryCreated: () => {},
+  categoryUpdated: () => {},
+  categoryDeleted: () => {},
+};
